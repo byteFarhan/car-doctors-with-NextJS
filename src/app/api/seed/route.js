3f7dbd7eebea23d0b17dadcf5a275a36f7999953
farhan-mazumder <farhanmazumder75@gmail.com>
2024-10-20 15:12:00 +0600

    Create seed api for inserting datas in mongoDB collection.

--- src/app/api/seed/route.js
+++ src/app/api/seed/route.js
@@ -1,1 +1,15 @@
-export async function POST() {}
+import { connectDB } from "@/lib/connectDB";
+import { services } from "@/../public/data/services";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const db = await connectDB();
+  const topServicesCollection = db.collection("top-services");
+  try {
+    await topServicesCollection.deleteMany();
+    const resp = await topServicesCollection.insertMany(services);
+    return NextResponse.json({ message: "Seeded successfully.", resp });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
