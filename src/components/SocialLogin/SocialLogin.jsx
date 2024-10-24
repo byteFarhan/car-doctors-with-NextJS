b8709eca2e4f8a1d11b68e902f55c8beec33cceb
farhan-mazumder <farhanmazumder75@gmail.com>
2024-10-24 09:24:04 +0600

    modified SocialLogin component.

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -3,48 +3,24 @@ import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import googleIcon from "@/../public/assets/icons/google-icon.png";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
-// import { useContext } from "react";
-// import { AuthContext } from "../../../Providers/AuthProvider";
-// import swal from "sweetalert";
-// import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SocialLogin = () => {
-  //   const { signInwithGoogle } = useContext(AuthContext);
-  //   const navigate = useNavigate();
-  //   const handleFacebookLogin = () => {
-  //     // console.log("feature Comming Soon...");
-  //     swal("feature Comming Soon...");
-  //   };
-  //   const handleLinkedInLogin = () => {
-  //     // console.log("feature Comming Soon...");
-  //     swal("feature Comming Soon...");
-  //   };
-  //   const handleGoogleLogin = () => {
-  //     signInwithGoogle()
-  //       .then((result) => {
-  //         swal("Good job!", "Sing In Successfull.", "success");
-  //         navigate(location?.state ? location.state : "/");
-  //       })
-  //       .catch((error) => {
-  //         swal({
-  //           title: error.message,
-  //         });
-  //       });
-  //   };
+  const handleLinkedInLogin = () => {
+    Swal.fire({
+      position: "top-end",
+      icon: "info",
+      title: "Feature Comming Soon. Try another way!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
   return (
     <div>
-      {/* divder */}
-      {/* <div className="flex flex-col w-full border-opacity-50">
-        <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
-          content
-        </div>
-        <div className="divider">Or Sign In With</div>
-        <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
-          content
-        </div>
-      </div> */}
+      <div className="w-2/3 mx-auto mb-3 divider">Or Sign In With</div>
       <div className="my-5">
-        <p className="mb-3 text-center">Or Sign In with</p>
+        {/* <p className="mb-3 text-center">Or Sign In with</p> */}
         <ul className="flex justify-center *:cursor-pointer gap-5 items-center my-3 text-2xl  [&>*]:p-2 [&>*]:text-blue-600 [&>*]:bg-slate-100 [&>*]:rounded-full">
           <li onClick={() => signIn("google", { redirect: false })}>
             <Image
@@ -58,9 +34,7 @@ const SocialLogin = () => {
           <li onClick={() => signIn("facebook")}>
             <FaFacebookF />
           </li>
-          <li
-          //   onClick={handleLinkedInLogin}
-          >
+          <li onClick={handleLinkedInLogin}>
             <FaLinkedinIn />
           </li>
         </ul>
