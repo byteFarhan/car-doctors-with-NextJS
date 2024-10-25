c3f4db4dc50fb185118eabf8ccaf29c1e4bb8beb
farhan-mazumder <farhanmazumder75@gmail.com>
2024-10-25 07:13:14 +0600

    Check Out page created.

---
+++ src/app/checkout/page.jsx
@@ -0,0 +1,12 @@
+import PageTitle from "@/components/PageTitle/PageTitle";
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <section className="container">
+      <PageTitle title="Check Out" pagePath="Home/Checkout" />
+    </section>
+  );
+};
+
+export default Checkout;
