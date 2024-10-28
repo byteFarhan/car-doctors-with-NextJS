c3532b98b1150e7151fd57a5012ebe58bcc97c86
farhan-mazumder <farhanmazumder75@gmail.com>
2024-10-28 13:14:01 +0600

    fetch booked services based on user email.

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -2,7 +2,38 @@
 import PageTitle from "@/components/PageTitle/PageTitle";
 import titleBG from "@/../public/assets/images/checkout/checkout.png";
 
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+
 const MyBookings = () => {
+  const [bookings, setBookings] = useState([]);
+  const session = useSession();
+  const user = session?.data?.user;
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const resp = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/booking-service?_email=${user?.email}`
+        );
+
+        if (!resp.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await resp.json();
+        // console.log(data);
+        setBookings(data);
+      } catch (error) {
+        console.error("Error fetching bookings:", error);
+      }
+    };
+
+    if (user?.email) {
+      fetchBookings();
+    }
+  }, [user]);
+
   return (
     <section className="container">
       <PageTitle
